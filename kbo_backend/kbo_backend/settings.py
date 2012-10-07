d801e7faaccbb5b0ac8a653daecfcdea88d084ab
danielgur <danielgur2105@gmail.com>
2012-10-07 01:05:04 -0400

    adding comments

--- kbo_backend/kbo_backend/settings.py
+++ kbo_backend/kbo_backend/settings.py
@@ -19,8 +19,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')}
-"""'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(SITE_ROOT, 'backend.db'), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
