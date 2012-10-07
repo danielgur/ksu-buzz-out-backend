26de26696c95805f7eb1839dd114a9f44c284a68
danielgur <danielgur2105@gmail.com>
2012-10-07 00:29:50 -0400

    my django app

--- kbo_backend/kbo_backend/settings.py
+++ kbo_backend/kbo_backend/settings.py
@@ -1,6 +1,7 @@
 author__ = "danielgur2105@gmail.com (Daniel Gur)"
 
 import os
+import dj_database_url
 import django
 
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
@@ -18,15 +19,15 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    'default': dj_database_url.config(default='postgres://localhost')}
+"""'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(SITE_ROOT, 'backend.db'), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
-}
+}"""
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
