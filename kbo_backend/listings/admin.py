b3324c7c8cf84bdcb1a8039b6847d9f0da3f592c
danielgur <danielgur2105@gmail.com>
2012-10-12 15:31:15 -0400

    pushing to heroku

--- kbo_backend/listings/admin.py
+++
@@ -1,9 +0,0 @@
-author__ = "danielgur2105@gmail.com (Daniel Gur)"
-
-from listings.models import Location, Category
-from django.contrib import admin
-from django.contrib.sites.models import Site
-
-admin.site.register(Location)
-admin.site.register(Category)
-admin.site.unregister(Site)
